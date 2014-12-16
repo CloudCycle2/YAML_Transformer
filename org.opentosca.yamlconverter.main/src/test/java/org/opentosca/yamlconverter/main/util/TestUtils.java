ac73230964d5a5bab2995f551a835ac05af9716d
iaavo <a.flaig@gmail.com>
2014-12-16 13:14:26 +0100

    Added test to check the whole yaml hello world test

--- org.opentosca.yamlconverter.main/src/test/java/org/opentosca/yamlconverter/main/util/TestUtils.java
+++ org.opentosca.yamlconverter.main/src/test/java/org/opentosca/yamlconverter/main/util/TestUtils.java
@@ -1,7 +1,9 @@
 package org.opentosca.yamlconverter.main.util;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.URISyntaxException;
 
 import org.apache.commons.io.FileUtils;
@@ -14,5 +16,28 @@ public class TestUtils {
 	public String readYamlTestResource(String path) throws URISyntaxException, IOException {
 		final File simpleYaml = new File(getClass().getResource(path).toURI());
 		return FileUtils.readFileToString(simpleYaml);
+	}
+
+	/**
+	 * Checks whether the given yaml string and the yaml content in the given path are equal. The yaml content is stripped of comments.
+	 * 
+	 * @param yaml
+	 * @param path
+	 * @return
+	 */
+	public boolean yamlFilesEqual(String yaml, String path) {
+		try (final BufferedReader bis = new BufferedReader(new InputStreamReader(getClass().getResourceAsStream(path), "UTF-8"))) {
+			final StringBuffer yaml2 = new StringBuffer();
+			String line = "";
+			while ((line = bis.readLine()) != null) {
+				if (!line.trim().startsWith("#")) {
+					yaml2.append(line);
+				}
+			}
+			return yaml2.toString().equals(yaml2);
+		} catch (final IOException e) {
+			return false;
+		}
+
 	}
 }
--- org.opentosca.yamlconverter.yamlmodel/src/org/opentosca/yamlconverter/yamlmodel/yaml/element/YAMLElement.java
+++ org.opentosca.yamlconverter.yamlmodel/src/org/opentosca/yamlconverter/yamlmodel/yaml/element/YAMLElement.java
@@ -17,4 +17,23 @@ public abstract class YAMLElement {
 			this.description = description;
 		}
 	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (obj == null) {
+			return false;
+		}
+		if (this == obj) {
+			return true;
+		}
+		if (obj instanceof YAMLElement) {
+			final YAMLElement other = (YAMLElement) obj;
+			if (this.description == null) {
+				return other.description == null;
+			} else {
+				return this.description.equals(other.description);
+			}
+		}
+		return false;
+	}
 }
