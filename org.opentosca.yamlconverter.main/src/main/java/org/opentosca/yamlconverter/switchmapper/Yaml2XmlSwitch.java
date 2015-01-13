c09c70213a535b32c2be54c56f4b50a216d32199
Jonas Heinisch <jonas@heinisch-web.de>
2015-01-13 12:20:21 +0100

    more mapping (untested)

--- org.opentosca.yamlconverter.main/src/main/java/org/opentosca/yamlconverter/switchmapper/Yaml2XmlSwitch.java
+++ org.opentosca.yamlconverter.main/src/main/java/org/opentosca/yamlconverter/switchmapper/Yaml2XmlSwitch.java
@@ -5,7 +5,10 @@ import java.util.Map;
 import javax.xml.namespace.QName;
 
 import org.opentosca.model.tosca.Definitions;
+import org.opentosca.model.tosca.TEntityTemplate;
 import org.opentosca.model.tosca.TNodeTemplate;
+import org.opentosca.model.tosca.TServiceTemplate;
+import org.opentosca.model.tosca.TTopologyTemplate;
 import org.opentosca.yamlconverter.yamlmodel.yaml.element.NodeTemplate;
 import org.opentosca.yamlconverter.yamlmodel.yaml.element.PropertyType;
 import org.opentosca.yamlconverter.yamlmodel.yaml.element.YAMLFileRoot;
@@ -25,6 +28,8 @@ public class Yaml2XmlSwitch {
 
 	private Definitions case_YAMLFileRoot(YAMLFileRoot elem) {
 		final Definitions result = new Definitions();
+		final TServiceTemplate serviceTemplate = new TServiceTemplate();
+		final TTopologyTemplate topologyTemplate = new TTopologyTemplate();
 		result.setId(unique("root"));
 		result.setName(unique("Root"));
 		// result.setTargetNamespace();
@@ -32,25 +37,55 @@ public class Yaml2XmlSwitch {
 		// result.setTypes();
 		// result.getDocumentation().add(docu);
 		// result.getOtherAttributes().put(name, attribute);
+		/*
+		 * for(CapabilityType capType : elem.getCapabilityTypes()){
+		 * result.getServiceTemplateOrNodeTypeOrNodeTypeImplementation().add(case_CapabilityType(capType)); }
+		 */
+		/*
+		 * for(NodeType nodeType : elem.getNodeTypes()){
+		 * result.getServiceTemplateOrNodeTypeOrNodeTypeImplementation().add(case_NodeType(nodeType)); }
+		 */
+		/*
+		 * for(RelationshipType relType : elem.getRelationshipTypes()){
+		 * result.getServiceTemplateOrNodeTypeOrNodeTypeImplementation().add(case_RelationshipType(relType)); }
+		 */
+		/*
+		 * for(ArtifactType artType : elem.getArtifactType()){
+		 * result.getServiceTemplateOrNodeTypeOrNodeTypeImplementation().add(case_ArtifactType(artType)); }
+		 */
+		/*
+		 * for (final Import importelem : elem.getImports()) { result.getImport().add(case_Import(importelem)); }
+		 */
+		// serviceTemplate.setBoundaryDefinitions(value);
+		serviceTemplate.setId(unique("serviceTemplate"));
+		serviceTemplate.setName("ServiceTemplate");
+		// serviceTemplate.setPlans(value);
+		// serviceTemplate.setSubstitutableNodeType(value);
+		// serviceTemplate.setTags(value);
+		// serviceTemplate.setTargetNamespace(value);
+		serviceTemplate.setTopologyTemplate(topologyTemplate);
+		// serviceTemplate.getDocumentation().add(docu);
+		// serviceTemplate.getOtherAttributes().put(key, value);
+		// topologyTemplate.getAny().add(o);
+		// topologyTemplate.getDocumentation().add(docu);
 		for (final Map.Entry<String, NodeTemplate> nt : elem.getNode_templates().entrySet()) {
 			final TNodeTemplate xnode = case_NodeTemplate(nt.getValue());
 			// override name and id of the nodetemplate
 			xnode.setName(nt.getKey());
 			xnode.setId(name2id(nt.getKey()));
-			result.getServiceTemplateOrNodeTypeOrNodeTypeImplementation().add(xnode);
+			topologyTemplate.getNodeTemplateOrRelationshipTemplate().add(xnode);
 		}
-		// The import is the last one, because we may need to add some resulting from other fields.
-		// result.getImport().add(import);
+		// topologyTemplate.getOtherAttributes().put(key, value);
 		return result;
 	}
 
 	@SuppressWarnings("unused")
 	private Object case_Propertytype(PropertyType elem) {
+		// TODO
 		return null;
 	}
 
 	private TNodeTemplate case_NodeTemplate(NodeTemplate elem) {
-		// TODO: elem.getProperties()
 		final TNodeTemplate result = new TNodeTemplate();
 		// result.setCapabilities(cap);
 		// result.setDeploymentArtifacts(depa);
@@ -59,7 +94,10 @@ public class Yaml2XmlSwitch {
 		// result.setMinInstances(mininst);
 		result.setName(unique("Nodetemplate"));
 		// result.setPolicies(poli);
-		// result.setProperties(prop);
+		final TEntityTemplate.Properties prop = new TEntityTemplate.Properties();
+		final Object properties = parseProperties(elem.getProperties(), elem.getType());
+		prop.setAny(properties);
+		result.setProperties(prop);
 		// result.setPropertyConstraints(propconstr);
 		// result.setRequirements(req);
 		result.setType(new QName(elem.getType()));
@@ -67,6 +105,11 @@ public class Yaml2XmlSwitch {
 		// result.getDocumentation().add(docu);
 		// result.getOtherAttributes().put(name, attr)
 		return result;
+	}
+
+	private Object parseProperties(Map<String, Object> properties, String type) {
+		// TODO: implement this in the right way...
+		return null;
 	}
 
 	/**
