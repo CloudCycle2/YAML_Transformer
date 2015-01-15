799328ef741dba370216c431d29db55265c0dec1
iaavo <a.flaig@gmail.com>
2015-01-15 17:58:14 +0100

    Xml Lax Test added

--- org.opentosca.yamlconverter.main/src/test/java/org/opentosca/yamlconverter/main/XmlAnyLaxTest.java
+++ org.opentosca.yamlconverter.main/src/test/java/org/opentosca/yamlconverter/main/XmlAnyLaxTest.java
@@ -1,10 +1,14 @@
 package org.opentosca.yamlconverter.main;
 
 import java.io.ByteArrayInputStream;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
 
 import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBElement;
 import javax.xml.bind.Marshaller;
 import javax.xml.bind.Unmarshaller;
 import javax.xml.bind.annotation.XmlAccessType;
@@ -12,19 +16,21 @@ import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlAnyElement;
 import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.namespace.QName;
 
 public class XmlAnyLaxTest {
 
 	// @formatter:off
-	private static final String xml = "<message to=\"john@example.com\" from=\"jane@example.com\">" +
-			"<customer>" +
-			"<name>Sue Smith</name>" +
-			"<address>" +
-			"<street>123 A Street</street>" +
-			"<city>Any Town</city>" +
-			"</address>" +
-			"</customer>" +
-			"</message>";
+	private static final String xml =
+			"<message to=\"john@example.com\" from=\"jane@example.com\">" +
+					"<customer>" +
+					"<name>Sue Smith</name>" +
+					"<address>" +
+					"<street>123 A Street</street>" +
+					"<city>Any Town</city>" +
+					"</address>" +
+					"</customer>" +
+					"</message>";
 	// @formatter:on
 
 	public static void main(String[] args) throws Exception {
@@ -34,10 +40,10 @@ public class XmlAnyLaxTest {
 		final Unmarshaller unmarshaller = jc.createUnmarshaller();
 		final Message message = (Message) unmarshaller.unmarshal(new ByteArrayInputStream(xml.getBytes()));
 
-		// TODO Does not work!
-		final GenericProperties props = new GenericProperties();
-		props.properties = new HashMap<String, String>();
-		props.properties.put("lol", "yeah");
+		final Map<String, String> map = new LinkedHashMap<String, String>();
+		map.put("key1", "value1");
+		map.put("key2", "value2");
+		final GenericProperties props = new GenericProperties(map);
 
 		message.body = props;
 
@@ -46,12 +52,19 @@ public class XmlAnyLaxTest {
 		marshaller.marshal(message, System.out);
 	}
 
-	@XmlRootElement
-	@XmlAccessorType(XmlAccessType.FIELD)
+	@XmlRootElement(name = "Properties")
 	static class GenericProperties {
+		@XmlAnyElement
+		public List<JAXBElement<String>> entries = new ArrayList<JAXBElement<String>>();
 
-		public Map<String, String> properties;
+		public GenericProperties() { // JAXB required
+		}
 
+		public GenericProperties(Map<String, String> map) {
+			for (final Map.Entry<String, String> entry : map.entrySet()) {
+				this.entries.add(new JAXBElement<String>(new QName(entry.getKey()), String.class, entry.getValue()));
+			}
+		}
 	}
 
 	@XmlRootElement
