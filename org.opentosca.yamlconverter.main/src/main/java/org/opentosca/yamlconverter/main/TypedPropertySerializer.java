dc48db64ee98ba14d2e838037f04ba417dd1c9c6
iaavo <a.flaig@gmail.com>
2015-01-27 16:27:28 +0100

    TypedPropertySerializer added

---
+++ org.opentosca.yamlconverter.main/src/main/java/org/opentosca/yamlconverter/main/TypedPropertySerializer.java
@@ -0,0 +1,22 @@
+package org.opentosca.yamlconverter.main;
+
+import org.opentosca.yamlconverter.yamlmodel.yaml.element.TypedProperty;
+
+import com.esotericsoftware.yamlbeans.YamlException;
+import com.esotericsoftware.yamlbeans.scalar.ScalarSerializer;
+
+public class TypedPropertySerializer implements ScalarSerializer<TypedProperty> {
+
+	@Override
+	public String write(TypedProperty object) throws YamlException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public TypedProperty read(String value) throws YamlException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
