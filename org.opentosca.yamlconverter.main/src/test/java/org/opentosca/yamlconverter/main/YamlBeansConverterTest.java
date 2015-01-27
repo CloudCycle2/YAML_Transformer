83cbd36055b622bff33b58c01911683cb73ad763
Sebastian Hesse <sebi-hesse1@gmx.de>
2015-01-27 15:11:42 +0100

    adjusted input class to support inputs in yaml files and created a test for this case

--- org.opentosca.yamlconverter.main/src/test/java/org/opentosca/yamlconverter/main/YamlBeansConverterTest.java
+++ org.opentosca.yamlconverter.main/src/test/java/org/opentosca/yamlconverter/main/YamlBeansConverterTest.java
@@ -3,7 +3,10 @@ package org.opentosca.yamlconverter.main;
 import org.junit.Assert;
 import org.junit.Test;
 import org.opentosca.yamlconverter.main.interfaces.IToscaYaml2YamlBeanConverter;
+import org.opentosca.yamlconverter.yamlmodel.yaml.element.Input;
 import org.opentosca.yamlconverter.yamlmodel.yaml.element.ServiceTemplate;
+
+import java.util.Map;
 
 /**
  * @author Sebi
@@ -44,5 +47,18 @@ public class YamlBeansConverterTest extends BaseTest {
 	@Test(expected = IllegalArgumentException.class)
 	public void testYamlBean2Yaml_Null() throws Exception {
 		this.converter.yamlbean2yaml(null);
+	}
+
+	@Test
+	public void testReadYamlInputs() throws Exception {
+		final ServiceTemplate templ = this.converter.yaml2yamlbean(this.testUtils.readYamlTestResource("/yaml/inputs.yaml"));
+		Assert.assertNotNull(templ);
+		final Map<String, Input> inputs = templ.getInputs();
+		Assert.assertNotNull(inputs);
+		Assert.assertEquals("template must have 1 input", inputs.size(), 1);
+		final Input fooInput = inputs.get("foo");
+		Assert.assertEquals("fooInput must have the type 'string'", fooInput.getType(), "string");
+		Assert.assertEquals("fooInput must have 1 constraint", fooInput.getConstraints().size(), 2);
+		Assert.assertEquals("constraint min_length is equals 2", fooInput.getConstraints().get(0).get("min_length"), "2");
 	}
 }
