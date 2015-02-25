48cdc225b879f6e4c049611d6cf1e41bf9555ece
Sebastian Hesse <sebi-hesse1@gmx.de>
2015-02-25 14:37:47 +0100

    added derived_from to CapabilityType

--- org.opentosca.yamlconverter.yamlmodel/src/org/opentosca/yamlconverter/yamlmodel/yaml/element/CapabilityType.java
+++ org.opentosca.yamlconverter.yamlmodel/src/org/opentosca/yamlconverter/yamlmodel/yaml/element/CapabilityType.java
@@ -6,6 +6,7 @@ import java.util.Map;
 public class CapabilityType extends YAMLElement {
 
 	private Map<String, PropertyDefinition> properties = new HashMap<String, PropertyDefinition>();
+	private String derived_from = "";
 
 	public Map<String, PropertyDefinition> getProperties() {
 		return properties;
@@ -15,6 +16,15 @@ public class CapabilityType extends YAMLElement {
 		this.properties = properties;
 	}
 
+	public String getDerived_from() {
+		return derived_from;
+	}
+
+	public void setDerived_from(String derived_from) {
+		this.derived_from = derived_from;
+	}
+
+
 	@Override
 	public boolean equals(Object o) {
 		if (this == o) return true;
@@ -23,6 +33,7 @@ public class CapabilityType extends YAMLElement {
 
 		CapabilityType that = (CapabilityType) o;
 
+		if (!derived_from.equals(that.derived_from)) return false;
 		if (!properties.equals(that.properties)) return false;
 
 		return true;
@@ -32,6 +43,7 @@ public class CapabilityType extends YAMLElement {
 	public int hashCode() {
 		int result = super.hashCode();
 		result = 31 * result + properties.hashCode();
+		result = 31 * result + derived_from.hashCode();
 		return result;
 	}
 }
