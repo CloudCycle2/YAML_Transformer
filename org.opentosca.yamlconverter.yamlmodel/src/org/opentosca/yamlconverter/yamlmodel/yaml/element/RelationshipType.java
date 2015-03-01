5218614528126404d94ca82511f13255236842c2
Sebastian Hesse <sebi-hesse1@gmx.de>
2015-03-01 18:41:52 +0100

    added derived_from to RelationshipType

--- org.opentosca.yamlconverter.yamlmodel/src/org/opentosca/yamlconverter/yamlmodel/yaml/element/RelationshipType.java
+++ org.opentosca.yamlconverter.yamlmodel/src/org/opentosca/yamlconverter/yamlmodel/yaml/element/RelationshipType.java
@@ -6,10 +6,21 @@ import java.util.Map;
 
 public class RelationshipType extends YAMLElement {
 
+	private String derived_from = "";
 	private Map<String, PropertyDefinition> properties = new HashMap<String, PropertyDefinition>();
 	private Map<String, Map<String, Map<String, String>>> interfaces =
 			new HashMap<String, Map<String, Map<String, String>>>();
-	private String[] valid_targets;
+	private String[] valid_targets = new String[0];
+
+	public String getDerived_from() {
+		return derived_from;
+	}
+
+	public void setDerived_from(String derived_from) {
+		if (derived_from != null) {
+			this.derived_from = derived_from;
+		}
+	}
 
 	public Map<String, PropertyDefinition> getProperties() {
 		return properties;
@@ -49,6 +60,7 @@ public class RelationshipType extends YAMLElement {
 
 		RelationshipType that = (RelationshipType) o;
 
+		if (!derived_from.equals(that.derived_from)) return false;
 		if (!interfaces.equals(that.interfaces)) return false;
 		if (!properties.equals(that.properties)) return false;
 		if (!Arrays.equals(valid_targets, that.valid_targets)) return false;
@@ -59,9 +71,10 @@ public class RelationshipType extends YAMLElement {
 	@Override
 	public int hashCode() {
 		int result = super.hashCode();
+		result = 31 * result + derived_from.hashCode();
 		result = 31 * result + properties.hashCode();
 		result = 31 * result + interfaces.hashCode();
-		result = 31 * result + (valid_targets != null ? Arrays.hashCode(valid_targets) : 0);
+		result = 31 * result + Arrays.hashCode(valid_targets);
 		return result;
 	}
 }
