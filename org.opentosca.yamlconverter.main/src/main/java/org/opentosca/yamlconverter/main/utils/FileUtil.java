0e678afae65f7072a00656f2c0365732dd5843a7
Jonas Heinisch <jonas@heinisch-web.de>
2015-03-04 10:54:38 +0100

    added csar build step to consoleUI

--- org.opentosca.yamlconverter.main/src/main/java/org/opentosca/yamlconverter/main/utils/FileUtil.java
+++ org.opentosca.yamlconverter.main/src/main/java/org/opentosca/yamlconverter/main/utils/FileUtil.java
@@ -1,6 +1,7 @@
 package org.opentosca.yamlconverter.main.utils;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.URISyntaxException;
 
@@ -9,7 +10,7 @@ import org.apache.commons.io.FileUtils;
 public class FileUtil {
 	/**
 	 * Read file from path and return String.
-	 * 
+	 *
 	 * @param path filepath
 	 * @return the file content
 	 * @throws URISyntaxException
@@ -22,12 +23,70 @@ public class FileUtil {
 
 	/**
 	 * Saves a String to a file.
-	 * 
+	 *
 	 * @param filename The filename.
 	 * @param content Content to write to file.
 	 * @throws IOException
 	 */
-	public void saveStringAsFile(String filename, String content) throws IOException {
+	public static void saveStringAsFile(String filename, String content) throws IOException {
 		FileUtils.writeStringToFile(new File(filename), content);
+	}
+
+	public static void deleteDirectory(File dir) throws IOException {
+		if (!dir.exists()) {
+			return;
+		}
+
+		cleanDirectory(dir);
+
+		if (!dir.delete()) {
+			final String message = "Unable to delete directory " + dir + ".";
+			throw new IOException(message);
+		}
+	}
+
+	public static void cleanDirectory(final File directory) throws IOException {
+		if (!directory.exists()) {
+			final String message = directory + " does not exist";
+			throw new IllegalArgumentException(message);
+		}
+
+		if (!directory.isDirectory()) {
+			final String message = directory + " is not a directory";
+			throw new IllegalArgumentException(message);
+		}
+
+		final File[] files = directory.listFiles();
+		if (files == null) { // null if security restricted
+			throw new IOException("Failed to list contents of " + directory);
+		}
+
+		IOException exception = null;
+		for (final File file : files) {
+			try {
+				forceDelete(file);
+			} catch (final IOException ioe) {
+				exception = ioe;
+			}
+		}
+
+		if (null != exception) {
+			throw exception;
+		}
+	}
+
+	public static void forceDelete(final File file) throws IOException {
+		if (file.isDirectory()) {
+			deleteDirectory(file);
+		} else {
+			final boolean filePresent = file.exists();
+			if (!file.delete()) {
+				if (!filePresent) {
+					throw new FileNotFoundException("File does not exist: " + file);
+				}
+				final String message = "Unable to delete file: " + file;
+				throw new IOException(message);
+			}
+		}
 	}
 }
