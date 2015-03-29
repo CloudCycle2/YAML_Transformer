1500c9893735c2c303d820bd6fd5429149d039ed
Sebastian Hesse <sebi-hesse1@gmx.de>
2015-03-29 13:36:09 +0200

    added documentation and rearranged some statements for better readability

--- org.opentosca.yamlconverter.main/src/main/java/org/opentosca/yamlconverter/switchmapper/CapabilityTypesSubSwitch.java
+++ org.opentosca.yamlconverter.main/src/main/java/org/opentosca/yamlconverter/switchmapper/CapabilityTypesSubSwitch.java
@@ -7,6 +7,9 @@ import org.opentosca.yamlconverter.yamlmodel.yaml.element.CapabilityType;
 import javax.xml.namespace.QName;
 import java.util.Map.Entry;
 
+/**
+ * This sub switch supports processing the capability types of a YAML service template.
+ */
 public class CapabilityTypesSubSwitch extends AbstractSubSwitch {
 
 	public CapabilityTypesSubSwitch(Yaml2XmlSwitch parentSwitch) {
@@ -17,23 +20,32 @@ public class CapabilityTypesSubSwitch extends AbstractSubSwitch {
 	public void process() {
 		if (getServiceTemplate().getCapability_types() != null) {
 			for (final Entry<String, CapabilityType> capType : getServiceTemplate().getCapability_types().entrySet()) {
-				final TCapabilityType ct = createCapabilityType(capType);
-				ct.setName(capType.getKey());
-				getDefinitions().getServiceTemplateOrNodeTypeOrNodeTypeImplementation().add(ct);
+				final TCapabilityType capabilityType = createCapabilityType(capType);
+				capabilityType.setName(capType.getKey());
+				getDefinitions().getServiceTemplateOrNodeTypeOrNodeTypeImplementation().add(capabilityType);
 			}
 		}
 	}
 
+	/**
+	 * Creates a Tosca capability type. Sets name and other properties which can be read from
+	 * {@link org.opentosca.yamlconverter.yamlmodel.yaml.element.CapabilityType}.
+	 *
+	 * @param capType entry of a capability type in YAML service template
+	 * @return Tosca capability type
+	 */
 	private TCapabilityType createCapabilityType(Entry<String, CapabilityType> capType) {
 		final TCapabilityType result = new TCapabilityType();
 		final CapabilityType capabilityType = capType.getValue();
+		final QName derivedFrom = getTypeMapperUtil().getCorrectTypeReferenceAsQName(capabilityType.getDerived_from(), ElementType.CAPABILITY_TYPE);
+
+		result.setName(capType.getKey());
+		result.setDerivedFrom(parseDerivedFrom(derivedFrom));
+		result.getDocumentation().add(toDocumentation(capabilityType.getDescription()));
+
 		if (capabilityType.getProperties() != null && !capabilityType.getProperties().isEmpty()) {
 			result.setPropertiesDefinition(parsePropertiesDefinition(capabilityType.getProperties(), capType.getKey()));
 		}
-		result.setName(capType.getKey());
-		final QName derivedFrom = getTypeMapperUtil().getCorrectTypeReferenceAsQName(capabilityType.getDerived_from(), ElementType.CAPABILITY_TYPE);
-		result.setDerivedFrom(parseDerivedFrom(derivedFrom));
-		result.getDocumentation().add(toDocumentation(capabilityType.getDescription()));
 		return result;
 	}
 
